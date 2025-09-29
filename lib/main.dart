c0b0a2db71b11d1edd09f38813016aea5ff98f1d
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-29 14:53:22 +0200

    startscreen: card, gradient button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 
 
@@ -34,34 +35,99 @@ class MainApp extends StatelessWidget {
 }
 
 
+
 class ScreenStart extends StatelessWidget {
   const ScreenStart({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: SafeArea(
-        child: Column(
-          children: [
+      // если AppBar не нужен — можно убрать и оставить только body
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/backgrounds/bg_startscreen.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 0, 40, 40),                
+                child: Center(
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 600),
+                    child: Card(
+                      color: Colors.black.withValues(alpha: 0.55),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      elevation: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 30),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Title",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 36,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 12),
+                            const Text(
+                              "oih oih ouh ouih oij pijrvfopo3irfjo3pifj po3roifj 3pr4ofj34pfo 3pfoj3f p3jf p2 fp3rfj poij r34fgpoi34j fp3fj 34pof3 f",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                height: 1.3,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 16),
 
-            Text("Startseite"),
-
-
-            FilledButton(
-              onPressed: () {
-                Navigator.pushNamed(context, Routes.screenMain);
-              },
-              child: const Text('Zu Hauptseite'),
-
-            ),
-          ],
-        )
-        
-        
+                            Container(
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                  colors: [Color(0xfff075d5), Color(0xfff59dbe)],
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                ),
+                                borderRadius: BorderRadius.circular(10),                                 
+                                ),
+                                child:FilledButton(
+                                  style: FilledButton.styleFrom(
+                                    backgroundColor: Colors.transparent,
+                                    shadowColor: Colors.transparent,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadiusGeometry.circular(10),
+                                    ),
+                                  ),                              
+                              onPressed: () {
+                                Navigator.pushNamed(context, Routes.screenMain);
+                              },
+                                
+                              child: const Text('Zu Hauptseite'),
+                                ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      
-      //("Screen 1"),
     );
   }
 }
@@ -70,13 +136,17 @@ class ScreenStart extends StatelessWidget {
 class ScreenMain extends StatelessWidget {
   const ScreenMain({super.key});
 
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: SafeArea(
-        child: Column(
-          children: [
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/backgrounds/bg_mainscreen.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: SafeArea(child: Column(
+        children: [
 
             Text("Hauptseite"),
 
@@ -86,15 +156,10 @@ class ScreenMain extends StatelessWidget {
                 Navigator.pushNamed(context, Routes.screenDetail);
               },
               child: const Text('Zu Detailseite'),
-              
+
             ),
           ],
-        )
-        
-        
-      ),
-      
-      
+      )),
     );
   }
 }
