2e715e4be5fc5f5c075f55c0c9d85a2b01da7a41
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-29 12:40:34 +0200

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+
+
+class Routes {
+  static const screenStart = '/';
+  static const screenMain = '/screenMain';
+  static const screenDetail = '/screenDetail';
+}
+
+
 void main() {
   runApp(const MainApp());
 }
@@ -7,14 +16,119 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+   
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp (
+      initialRoute: Routes.screenStart,
+    
+    
+    routes: {
+      Routes.screenStart: (_) => const ScreenStart(),
+      Routes.screenMain: (_) => const ScreenMain(),
+      Routes.screenDetail: (_) => const ScreenDetail(),
+    }
+    ); 
+  }
+  
+}
+
+
+class ScreenStart extends StatelessWidget {
+  const ScreenStart({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            Text("Startseite"),
+
+
+            FilledButton(
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.screenMain);
+              },
+              child: const Text('Zu Hauptseite'),
+
+            ),
+          ],
+        )
+        
+        
       ),
+      
+      //("Screen 1"),
+    );
+  }
+}
+
+
+class ScreenMain extends StatelessWidget {
+  const ScreenMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            Text("Hauptseite"),
+
+
+            FilledButton(
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.screenDetail);
+              },
+              child: const Text('Zu Detailseite'),
+              
+            ),
+          ],
+        )
+        
+        
+      ),
+      
+      
+    );
+  }
+}
+
+
+
+class ScreenDetail extends StatelessWidget {
+  const ScreenDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            Text("Einzelansicht"),
+
+
+            FilledButton(
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.screenStart);
+              },
+              child: const Text('zu Startseite'),
+              
+            ),
+          ],
+        )
+        
+        
+      ),
+      
+      
     );
   }
 }
